0dcbb9ddc82f356eec28fd32ee84debac6b310db
Haley Zeng <hzeng@stuy.edu>
2017-03-21 23:27:25 -0400

    yeah

--- graph.js
+++ graph.js
@@ -1,3 +1,4 @@
+var colors = ["red", "green", "yellow", "blue", "orange"]
 var obamaData = [["Individual Income Taxes", 43], ["Payroll Taxes", 42], ["Corporate Income Taxes", 7], ["Excise Taxes", 3], ["Other", 5]];
 
 d3.select("#obama")
@@ -5,8 +6,10 @@ d3.select("#obama")
     .data(obamaData)
     .enter().append("div")
     .style("width", function(d) {
-	return d[1]*10 + "px"; })
-    .text(function(d) {return d[0]; });
+	return d[1]/2 + "%"; })
+    .text(function(d) {return d[0]; })
+    .style("background-color", function(d, i) {
+	return colors[i]});
 
 
 var almostTrumpData = [["Individual Income Taxes", 49], ["Payroll Taxes", 33], ["Corporate Income Taxes", 9], ["Excise Taxes", 3], ["Other", 7]];
@@ -16,5 +19,7 @@ d3.select("#almostTrump")
     .data(almostTrumpData)
     .enter().append("div")
     .style("width", function(d) {
-	return d[1]*10 + "px"; })
-    .text(function(d) {return d[0]; });
+	return d[1]/2 + "%"; })
+    .text(function(d) {return d[0]; })
+    .style("background-color", function(d, i) {
+	return colors[i]});
